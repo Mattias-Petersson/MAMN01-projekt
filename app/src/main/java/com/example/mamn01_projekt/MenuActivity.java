7635e82a598823cadb99796e735b81e328ff6a6f
Mattias Petersson <Mattias.Petersson93@gmail.com>
2022-04-06 14:15:02 +0200

    Added a menu, refactored MainActivity to GameActivity
    Changed so that the game is within a button, added a main menu with said button. Added onClick() to said button to bring back to the 'game'.

---
+++ app/src/main/java/com/example/mamn01_projekt/MenuActivity.java
@@ -0,0 +1,20 @@
+package com.example.mamn01_projekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+     public void startGame(View v) {
+        startActivity(new Intent(this, GameActivity.class));
+    }
+}
