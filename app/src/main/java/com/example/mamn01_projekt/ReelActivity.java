e4c0d019f6b1562200d265ea42ff63aa1a798754
Mattias Petersson <Mattias.Petersson93@gmail.com>
2022-04-06 16:05:49 +0200

    added counter for amount of reel-laps
    Added a counter along with two booleans that check how many laps the user has spun the reel in. Might be useful later. Will need to check in with others in group for the TODO.

--- app/src/main/java/com/example/mamn01_projekt/ReelActivity.java
+++ app/src/main/java/com/example/mamn01_projekt/ReelActivity.java
@@ -8,11 +8,16 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ReelActivity extends AppCompatActivity implements View.OnTouchListener {
 
     private ImageView reelImage;
+    private TextView text;
     private double currentAngle = 0;
+    private int counter = 0;
+    private boolean pointingUp = true;
+    private boolean pointingDown = false;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -20,7 +25,9 @@ public class ReelActivity extends AppCompatActivity implements View.OnTouchListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reel);
         reelImage = findViewById(R.id.imageReel);
+        text = findViewById(R.id.textView);
         reelImage.setOnTouchListener(this);
+
 
     }
 
@@ -34,16 +41,19 @@ public class ReelActivity extends AppCompatActivity implements View.OnTouchListe
             case MotionEvent.ACTION_MOVE:
                 double startAngle = currentAngle;
                 currentAngle = getAngle(event.getX(), event.getY());
+                checkLaps(currentAngle);
                 animate(startAngle, currentAngle);
                 break;
         }
         return true;
     }
+
     private double getAngle(double x, double y) {
         double xc = reelImage.getWidth() / 2.0;
         double yc = reelImage.getHeight() / 2.0;
         return Math.toDegrees(Math.atan2(x - xc, yc - y));
     }
+
     private void animate(double fromDegrees, double toDegrees) {
         RotateAnimation rotate = new RotateAnimation((float) fromDegrees, (float) toDegrees,
                 RotateAnimation.RELATIVE_TO_SELF, 0.5f,
@@ -52,6 +62,24 @@ public class ReelActivity extends AppCompatActivity implements View.OnTouchListe
         rotate.setFillEnabled(true);
         rotate.setFillAfter(true);
         reelImage.startAnimation(rotate);
+    }
+
+    //TODO ska man kunna snurra åt båda hållen eller är ett bakåt och ett framåt? Just nu kan man snurra åt båda
+    private void checkLaps(double angle) {
+        angle = (angle + 360) % 360; // Gets the angle in 0..360 instead of -180..180.
+        if (angle < 30 || angle > 330)
+            pointingUp = true;
+        // Reset the pointingUp so that users have to go down then up and not only register halves.
+        if ((angle < 120 && angle > 50) || (angle < 210 && angle > 150))
+            pointingUp = false;
+        if (angle < 210 && angle > 150)
+            pointingDown = true;
+
+        if (pointingUp && pointingDown) {
+            counter++;
+            pointingUp = pointingDown = false;
+        }
+        text.setText("Antal varv snurrade: " + counter);
     }
 }
 
